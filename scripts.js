01390ca04860a5639ee4dd442443bbee099b7537
Randy Parker <randallbparker@gmail.com>
2020-08-25 19:58:59 -0500

    mission abort listener and confirmation

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ function init(){
 
     const takeoff = document.getElementById("takeoff");
     const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
     const flightStatus = document.getElementById("flightStatus");
     const shuttleFlightScreen = document.getElementById("shuttleBackground");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
@@ -37,6 +38,19 @@ function init(){
 
         // The shuttle height should go down to 0.
         spaceShuttleHeight.innerHTML = 0;
+    });
+
+
+        // When the "Abort Mission" button is clicked, the following should happen:
+    missionAbort.addEventListener("click", function(){
+
+        // A window confirm should let the user know "Confirm that you want to abort the mission." If the user wants to abort the mission, then add parts b-d.
+        if (confirm("Confirm that you want to abort the mission.")){
+            
+        // The flight status should change to "Mission aborted."
+        // The background color of the shuttle flight screen should change from blue to green.
+        // The shuttle height should go to 0.
+        };
     });
 }
 
