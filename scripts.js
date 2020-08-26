68a85c72bb06c0dbf2c0db8f73e02bf0b755411a
Randy Parker <randallbparker@gmail.com>
2020-08-25 19:38:15 -0500

    onload and takeoff listener with confirm

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init(){
+    let confirmLiftoff;
+
+    const takeoff = document.getElementById("takeoff");
+
+    takeoff.addEventListener("click", function(event){
+        // do something
+        confirmLiftoff = confirm("Confirm that the shuttle is ready for takeoff.");
+    });
+}
+
+window.onload = init;
