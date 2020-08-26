0903cd6334e6c1dff58a6a1c24dc09548213c6ca
Randy Parker <randallbparker@gmail.com>
2020-08-25 19:42:04 -0500

    on confirm shuttle in flight status text set

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 function init(){
-    let confirmLiftoff;
 
     const takeoff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
 
     takeoff.addEventListener("click", function(event){
         // do something
-        confirmLiftoff = confirm("Confirm that the shuttle is ready for takeoff.");
+        if (confirm("Confirm that the shuttle is ready for takeoff.")){
+
+        // The flight status should change to "Shuttle in flight."
+            flightStatus.innerHTML = "Shuttle in flight.";
+
+        // The background color of the shuttle flight screen (id = "shuttleBackground") should change from green to blue.
+        // The shuttle height should increase by 10,000 miles.
+        };
+
     });
 }
 
